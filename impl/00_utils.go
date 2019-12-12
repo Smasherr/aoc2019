bccb062f19dda9c8185c22a64bca2882774909e0
Smasher <soundcracker@gmail.com>
2019-12-12 21:46:10 +0100

    Day 12

--- impl/00_utils.go
+++ impl/00_utils.go
@@ -118,7 +118,23 @@ func NewPoint2D(x int, y int) Point2D {
 	return Point2D{x, y}
 }
 
-type Point3D struct {
-	Point2D
-	Z int
+// GCD finds greatest common divisor (GCD) via Euclidean algorithm
+func GCD(a, b int) int {
+	for b != 0 {
+		t := b
+		b = a % b
+		a = t
+	}
+	return a
+}
+
+// LCM finds Least Common Multiple (LCM) via GCD
+func LCM(a, b int, integers ...int) int {
+	result := a * b / GCD(a, b)
+
+	for i := 0; i < len(integers); i++ {
+		result = LCM(result, integers[i])
+	}
+
+	return result
 }
--- impl/12_jupiter_moons.go
+++ impl/12_jupiter_moons.go
@@ -6,69 +6,81 @@ import (
 )
 
 const format = "<x=%d, y=%d, z=%d>"
-const steps = 1000
 
 // Main12 solves Day12
 func Main12() {
 	lines, _ := ReadLines("../res/12_jupiter_moons.txt")
 	moons := make([]*Moon, len(lines))
+	initState := make([]*Moon, len(lines))
 	for i := 0; i < len(lines); i++ {
 		moons[i] = new(Moon)
-		fmt.Sscanf(lines[i], format, &moons[i].Pos.X, &moons[i].Pos.Y, &moons[i].Pos.Z)
+		initState[i] = new(Moon)
+		fmt.Sscanf(lines[i], format, &moons[i].Pos[0], &moons[i].Pos[1], &moons[i].Pos[2])
+		fmt.Sscanf(lines[i], format, &initState[i].Pos[0], &initState[i].Pos[1], &initState[i].Pos[2])
 	}
-	for i := 0; i < steps; i++ {
-		TimeStep(moons)
-		fmt.Println(TotalEnergy(moons))
+	for i := 0; i < 1000; i++ {
+		for a := 0; a < 3; a++ {
+			TimeStep(moons, a)
+		}
 	}
+	fmt.Println(TotalEnergy(moons))
+	steps := make(chan int, 3)
+	for a := 0; a < 3; a++ {
+		go func(axis int) {
+			for i := 0; true; i++ {
+				TimeStep(moons, axis)
+				if StatesEqual(moons, initState, axis) {
+					steps <- i + 1001
+					return
+				}
+			}
+		}(a)
+	}
+	fmt.Println(LCM(<-steps, <-steps, <-steps))
+}
+
+// StatesEqual checks the equality of two states of the universes for an axis
+func StatesEqual(state1 []*Moon, state2 []*Moon, axis int) bool {
+	toRet := true
+	for i := 0; i < len(state1); i++ {
+		if state1[i].Pos[axis] != state2[i].Pos[axis] || state1[i].Vel[axis] != state2[i].Vel[axis] {
+			toRet = false
+		}
+	}
+	return toRet
 }
 
 // Moon represents a moon with a position and velocity
 type Moon struct {
-	Pos Point3D
-	Vel Point3D
+	Pos [3]int
+	Vel [3]int
 }
 
 // ApplyGravity applies gravity between two moons
-func ApplyGravity(m1 *Moon, m2 *Moon) {
-	if m2.Pos.X > m1.Pos.X {
-		m1.Vel.X++
-		m2.Vel.X--
-	} else if m2.Pos.X < m1.Pos.X {
-		m1.Vel.X--
-		m2.Vel.X++
-	}
-	if m2.Pos.Y > m1.Pos.Y {
-		m1.Vel.Y++
-		m2.Vel.Y--
-	} else if m2.Pos.Y < m1.Pos.Y {
-		m1.Vel.Y--
-		m2.Vel.Y++
-	}
-	if m2.Pos.Z > m1.Pos.Z {
-		m1.Vel.Z++
-		m2.Vel.Z--
-	} else if m2.Pos.Z < m1.Pos.Z {
-		m1.Vel.Z--
-		m2.Vel.Z++
+func ApplyGravity(m1 *Moon, m2 *Moon, axis int) {
+	if m2.Pos[axis] > m1.Pos[axis] {
+		m1.Vel[axis]++
+		m2.Vel[axis]--
+	} else if m2.Pos[axis] < m1.Pos[axis] {
+		m1.Vel[axis]--
+		m2.Vel[axis]++
 	}
 }
 
 // ApplyVelocity applies velocity to a moon
-func ApplyVelocity(m *Moon) {
-	m.Pos.X += m.Vel.X
-	m.Pos.Y += m.Vel.Y
-	m.Pos.Z += m.Vel.Z
+func ApplyVelocity(m *Moon, axis int) {
+	m.Pos[axis] += m.Vel[axis]
 }
 
 // TimeStep applies gravity and velocity
-func TimeStep(moons []*Moon) {
+func TimeStep(moons []*Moon, axis int) {
 	for i := 0; i < len(moons); i++ {
 		for k := i + 1; k < len(moons); k++ {
-			ApplyGravity(moons[i], moons[k])
+			ApplyGravity(moons[i], moons[k], axis)
 		}
 	}
 	for _, m := range moons {
-		ApplyVelocity(m)
+		ApplyVelocity(m, axis)
 	}
 }
 
@@ -77,8 +89,8 @@ func TimeStep(moons []*Moon) {
 func TotalEnergy(moons []*Moon) int {
 	totalEnergy := 0
 	for _, m := range moons {
-		pot := int(math.Abs(float64(m.Pos.X)) + math.Abs(float64(m.Pos.Y)) + math.Abs(float64(m.Pos.Z)))
-		kin := int(math.Abs(float64(m.Vel.X)) + math.Abs(float64(m.Vel.Y)) + math.Abs(float64(m.Vel.Z)))
+		pot := int(math.Abs(float64(m.Pos[0])) + math.Abs(float64(m.Pos[1])) + math.Abs(float64(m.Pos[2])))
+		kin := int(math.Abs(float64(m.Vel[0])) + math.Abs(float64(m.Vel[1])) + math.Abs(float64(m.Vel[2])))
 		totalEnergy += pot * kin
 	}
 	return totalEnergy

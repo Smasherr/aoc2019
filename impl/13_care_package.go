9c405fabb38d81960aa533eefaab4a007b6083db
Daniel Estermann <est@ti8m.ch>
2019-12-13 13:41:18 +0100

    Day 13

--- impl/13_care_package.go
+++ impl/13_care_package.go
@@ -1,73 +1,100 @@
 package impl
 
 import (
-	"fmt"
 	"strconv"
 	"strings"
+	"time"
+
+	tm "github.com/buger/goterm"
 )
 
 // Main13 solves Day13
 func Main13() {
 	program := ReadProgram("../res/13_care_package.txt")
-	painter := newPainter()
-	ic := NewIntcomp(program, &painter, &painter)
-	ic.ProcessInstructions()
-	fmt.Println(painter.blocks)
+	game := newGame()
+	ic := NewIntcomp(program, &game, &game)
 	program[0] = 2
 	ic.ProcessInstructions()
-	fmt.Println(painter.score)
+	game.render()
 }
 
-type painter struct {
+type game struct {
 	paintedPoints map[Point2D]int
 	writePointer  int
 	currX         int
 	currY         int
 	blocks        int
 	joystick      int
-	ball          Point2D
+	paddle        Point2D
 	score         int
 }
 
-func newPainter() painter {
-	return painter{make(map[Point2D]int), 0, 0, 0, 0, 0, *new(Point2D), 0}
+func newGame() game {
+	return game{make(map[Point2D]int), 0, 0, 0, 0, 0, *new(Point2D), 0}
 }
 
-func (r *painter) Write(data []byte) (int, error) {
+func (g *game) Write(data []byte) (int, error) {
 	val, _ := strconv.Atoi(strings.TrimSpace(string(data)))
-	switch r.writePointer {
+	switch g.writePointer {
 	case 0:
-		r.currX = val
+		g.currX = val
 	case 1:
-		r.currY = val
+		g.currY = val
 	case 2:
-		if r.currX == -1 {
-			r.score = val
+		if g.currX == -1 {
+			g.score = val
 		} else {
-			if val == 2 {
-				r.blocks++
+			if val == 0 && g.paintedPoints[NewPoint2D(g.currX, g.currY)] == 2 {
+				g.blocks++
 			}
-			p := NewPoint2D(r.currX, r.currY)
-			r.paintedPoints[p] = val
+			if val == 3 {
+				g.paddle = NewPoint2D(g.currX, g.currY)
+			}
+			p := NewPoint2D(g.currX, g.currY)
+			g.paintedPoints[p] = val
 			if val == 4 {
-				if p.X > r.ball.X {
-					r.joystick = 1
-				} else if p.X < r.ball.X {
-					r.joystick = -1
+				if p.X > g.paddle.X {
+					g.joystick = 1
+				} else if p.X < g.paddle.X {
+					g.joystick = -1
 				} else {
-					r.joystick = 0
+					g.joystick = 0
 				}
-				r.ball = p
 			}
 		}
 	}
-	r.writePointer++
-	r.writePointer %= 3
+	g.writePointer++
+	g.writePointer %= 3
 	return len(data), nil
 }
 
-func (r *painter) Read(data []byte) (int, error) {
-	b := []byte(strconv.Itoa(r.joystick))
+func (g *game) Read(data []byte) (int, error) {
+	b := []byte(strconv.Itoa(g.joystick))
 	b = append(b, '\n')
+	g.render()
 	return copy(data, b), nil
+}
+
+func (g *game) render() {
+	tm.Clear()
+	tm.Printf("Score: %d\tBlocks: %d\n", g.score, g.blocks)
+	for y := 0; y <= 19; y++ {
+		for x := 0; x <= 43; x++ {
+			switch g.paintedPoints[NewPoint2D(x, y)] {
+			case 0:
+				tm.Print(" ")
+			case 1:
+				tm.Print("|")
+			case 2:
+				tm.Print("#")
+			case 3:
+				tm.Print("_")
+			case 4:
+				tm.Print("o")
+			}
+		}
+		tm.Println()
+	}
+	time.Sleep(25 * time.Millisecond)
+	tm.Flush()
 }
